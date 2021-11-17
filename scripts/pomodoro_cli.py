037580aa2b5040726ddcb031a1d33d7e59bf41d9
stivius <maxim11853@gmail.com>
2021-11-17 12:30:01 +0200

    Automatic backup 17-11-2021 12:30:01

--- scripts/pomodoro_cli.py
+++ scripts/pomodoro_cli.py
@@ -10,16 +10,16 @@ parser.add_argument('--init', help='initialize pomodro database', action='store_
 parser.add_argument('--projects', help='list of created projects', action='store_true')
 parser.add_argument('--add-project', metavar='project_name', help='add new project')
 
-parser.add_argument('--tasks', metavar='project_id', help='list of tasks in progress')
-parser.add_argument('--all-tasks', metavar='project_id', help='list of all tasks')
+parser.add_argument('--tasks', metavar='project_id', help='list of tasks in progress', type=int)
+parser.add_argument('--all-tasks', metavar='project_id', help='list of all tasks', type=int)
 parser.add_argument('--add-task', metavar=('project_id', 'task_name'), nargs=2, help='add new task to project')
-parser.add_argument('--remove-task', metavar='task_id')
-parser.add_argument('--finish-task', metavar='task_id')
+parser.add_argument('--remove-task', metavar='task_id', type=int)
+parser.add_argument('--finish-task', metavar='task_id', type=int)
 
-parser.add_argument('--start-pomo', metavar='task_id')
-parser.add_argument('--end-pomo', metavar='pomo_id')
+parser.add_argument('--start-pomo', metavar='task_id', type=int)
+parser.add_argument('--end-pomo', metavar='pomo_id', type=int)
 parser.add_argument('--end-current-pomo', action='store_true')
-parser.add_argument('--remove-pomo', metavar='pomo_id')
+parser.add_argument('--remove-pomo', metavar='pomo_id', type=int)
 parser.add_argument('--remove-current-pomo', action='store_true')
 parser.add_argument('--all-pomos', action='store_true')
 parser.add_argument('--active-pomos', action='store_true')
@@ -53,7 +53,7 @@ def list_projects(cursor):
     record = cursor.fetchall()
     for item in record:
         id, name = item
-        print(id, name)
+        print(id, name, sep='\t')
 
 def add_project(name, cursor):
     cursor.execute('''INSERT INTO projects (id, name) VALUES (NULL, '%s')''' % name)
@@ -69,7 +69,8 @@ def list_tasks(project_id, in_progress, cursor):
         record = cursor.fetchall()
         for item in record:
             id, name, done = item
-            print(id, name, done)
+            done = "In-Progress" if not done else "Done"
+            print(id, name, done, sep='\t')
 
 def get_name_by_id(table, id, cursor):
     cursor.execute('''SELECT name FROM {0} WHERE id='{1}' '''.format(table, id))
@@ -85,6 +86,13 @@ def get_last_id(table, cursor):
         return record[0][0]
     return -1
 
+def get_last_active_pomo(cursor):
+    cursor.execute('''SELECT max(id) FROM pomos WHERE end_dt IS NULL''')
+    record = cursor.fetchall()
+    if (len(record) != 0):
+        return record[0][0]
+    return -1
+
 def add_task(project_id, task_name, cursor):
     project_id = project_id if project_id != None else "NULL"
     cursor.execute('''INSERT INTO tasks (id, name, done, project_id) VALUES (NULL, '{0}', 0, {1})'''.format(task_name, project_id))
@@ -92,42 +100,56 @@ def add_task(project_id, task_name, cursor):
     print(get_last_id('tasks', cursor))
 
 def remove_task(task_id, cursor):
-    cursor.execute('''DELETE FROM tasks WHERE id={0}'''.format(task_id))
-    sqlite_connection.commit()
+    if task_id > 0:
+        cursor.execute('''DELETE FROM tasks WHERE id={0}'''.format(task_id))
+        sqlite_connection.commit()
+    else:
+        print("remove_task: task_id is invalid")
 
 def start_pomo(task_id, cursor):
-    now = datetime.datetime.now()
-    cursor.execute('''INSERT INTO pomos (id, start_dt, end_dt, task_id) VALUES (NULL, '{0}', NULL, {1})'''.format(now, task_id))
-    sqlite_connection.commit()
-    print(get_last_id('pomos', cursor))
+    if task_id > 0:
+        now = datetime.datetime.now().isoformat(sep=' ', timespec='seconds')
+        cursor.execute('''INSERT INTO pomos (id, start_dt, end_dt, task_id) VALUES (NULL, '{0}', NULL, {1})'''.format(now, task_id))
+        sqlite_connection.commit()
+        print(get_last_id('pomos', cursor))
+    else:
+        print("start_pomo: task_id is invalid")
 
 def end_pomo(pomo_id, cursor):
-    now = datetime.datetime.now()
-    cursor.execute('''UPDATE pomos SET end_dt='{0}' WHERE id={1} AND end_dt IS NULL '''.format(now, pomo_id))
-    sqlite_connection.commit()
+    if pomo_id > 0:
+        now = datetime.datetime.now().isoformat(sep=' ', timespec='seconds')
+        cursor.execute('''UPDATE pomos SET end_dt='{0}' WHERE id={1} AND end_dt IS NULL '''.format(now, pomo_id))
+        sqlite_connection.commit()
+    else:
+        print("end_pomo: pomo_id is invalid")
 
-def end_current_pomo(pomo_id, cursor):
-    current_pomo_id = get_last_id('pomos', cursor)
+def end_current_pomo(cursor):
+    current_pomo_id = get_last_active_pomo(cursor)
     end_pomo(current_pomo_id, cursor)
 
 def finish_task(task_id, cursor):
-    now = datetime.datetime.now()
-    cursor.execute('''UPDATE tasks SET done=1 WHERE id={0} '''.format(task_id))
-    sqlite_connection.commit()
+    if pomo_id > 0:
+        cursor.execute('''UPDATE tasks SET done=1 WHERE id={0} '''.format(task_id))
+        sqlite_connection.commit()
+    else:
+        print("finish_task: task_id is invalid")
 
 def remove_pomo(pomo_id, cursor):
-    cursor.execute('''DELETE FROM pomos WHERE id={0}'''.format(pomo_id))
-    sqlite_connection.commit()
+    if pomo_id > 0:
+        cursor.execute('''DELETE FROM pomos WHERE id={0}'''.format(pomo_id))
+        sqlite_connection.commit()
+    else:
+        print("remove_pomo: pomo_id is invalid")
 
 def remove_current_pomo(cursor):
-    current_pomo_id = get_last_id('pomos', cursor)
+    current_pomo_id = get_last_active_pomo(cursor)
     remove_pomo(current_pomo_id, cursor)
 
 def get_pomos(active_only, cursor):
     if not active_only:
         cursor.execute('''SELECT * FROM pomos''')
     else:
-        cursor.execute('''SELECT * FROM pomos WHERE end_dt is NULL''')
+        cursor.execute('''SELECT * FROM pomos WHERE end_dt IS NULL''')
     pomos = cursor.fetchall()
     for record in records:
         id, start_dt, end_dt, project_id, task_id = records
@@ -137,7 +159,7 @@ def get_pomos(active_only, cursor):
         print(id, start_dt, end_dt, project_name, task_name)
 
 try:
-    sqlite_connection = sqlite3.connect('pomodoro.db')
+    sqlite_connection = sqlite3.connect('/home/stivius/scripts/pomodoro.db')
     cursor = sqlite_connection.cursor()
 
     if (args.init):
